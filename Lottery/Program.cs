f467b71a5f1d1c1e9b40d36d7f703079a58b5c2d
CodySalerno <114028734+CodySalerno@users.noreply.github.com>
2022-09-21 14:39:39 -0500

    locked all instances of globals getting changed, added isStart to know if thread has started

--- Lottery/Program.cs
+++ Lottery/Program.cs
@@ -6,28 +6,25 @@ namespace Lottery
     class Program
     {
         private static readonly object coinLock = new(); //lock object
-        public static int activeThreadCount = -1; //how many games of the lottery are being played now
-        public static int totalThreadCount = -1; //how many total games have been started
-        public static int attempts = 1000; //total number of games meant to be played
+        public static int activeThreadCount = 0; //how many games of the lottery are being played now
+        public static int totalThreadCount = 0; //how many total games have been started
+        public static int attempts = 32; //total number of games meant to be played
         public static double[] allAttempts = new double[attempts]; //array of doubles with one index for every game
         public static string display = ""; //final display
         public static int togo = attempts; //how many games still need to be completed; starts at total attempt number
         public static Stopwatch SW = new(); //timer for how long completion takes
-        public static int current = -1;
+        public static bool isStarted = true;
         static void Main()
         {
             SW.Start();
-            while (totalThreadCount < (attempts -1)) //haven't started enough threads for every attempt
+            while (totalThreadCount < attempts) //haven't started enough threads for every attempt
             {
-                if (activeThreadCount < 16 && (totalThreadCount != current)) /* limits it to a max of 16 games being played at once
-                                                                              * second condition makes sure counts have been updated so as to not cause race conditions*/
+                if (activeThreadCount < 16 && (isStarted == true)) /* first condition limits it to a max of 16 games being played at once
+                                                                    * second conditions makes sure the previous thread has already started */
                 {
-                    
-                    current = totalThreadCount;
+                    isStarted = false;
                     Thread my_thread = new(() => StartZero(totalThreadCount)); /*starts a thread to run the StartZero function 
                                                                                 * with the number of threads before it as the index */
-                    activeThreadCount++;
-                    totalThreadCount++;
                     my_thread.Start();
                 }
             }
@@ -35,19 +32,19 @@ namespace Lottery
 
         static void StartZero(int index)
         {
-            if (index >= attempts)
+            lock (coinLock)
             {
-                Console.WriteLine("oops");
-                return;
+                activeThreadCount++;
+                totalThreadCount++;
             }
-            
             Console.WriteLine("Starting thread #" + index + " " + activeThreadCount + " threads are active");
+            isStarted = true;
             double runs = 0; //number of times an attempt has been made to win
             Random random = new(); //creates random number generator
             while (true)
             {
                 runs++; //every loop tries to get a 0
-                long coin1 = random.NextInt64(0, 2); // 0 or 1
+                long coin1 = random.NextInt64(0, 292201338); // odds of winning powerball, 0 is a success
                 if (coin1 == 0) //if success
                 {
                     lock (coinLock) //locks the object so no other thread can acess
@@ -58,7 +55,7 @@ namespace Lottery
                         Console.WriteLine("Thread #" + index + " finished after " + runs + " runs"); //shows what thread finished and how many attempts
                         if (togo == 0) //if all games have been completed
                         {
-                            // Array.Sort(allAttempts); //sorts array into luckiest to least lucky
+                            Array.Sort(allAttempts); //sorts array into luckiest to least lucky
                             display = String.Join(Environment.NewLine, allAttempts); //creates a string of all attempts
                             Console.WriteLine(display); //writes the string to console
                             SW.Stop(); //stops timer
